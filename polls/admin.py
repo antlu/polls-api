32608c87b13bb76caa38815d890efa6b02e1e69b
Anton <web-gambit@mail.ru>
2021-04-17 17:00:43 +0500

    Add models

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from polls.models import Answer, Poll, Question
+
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+
+
+@admin.register(Poll)
+class PollAdmin(admin.ModelAdmin):
+    list_display = ('title', 'start_time', 'end_time')
+    inlines = (QuestionInline,)
+
+
+class AnswerInline(admin.TabularInline):
+    model = Answer
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('text', 'poll')
+    inlines = (AnswerInline,)
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+TEXT_LENGTH = 255
+
+
+class Poll(models.Model):
+    title = models.CharField(max_length=TEXT_LENGTH)
+    start_time = models.DateTimeField(default=timezone.localtime)
+    end_time = models.DateTimeField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+    TYPES = (
+        ('text', 'Text'),
+        ('single_choice', 'Single choice'),
+        ('multi_choice', 'Multiple choice'),
+    )
+
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    text = models.CharField(max_length=TEXT_LENGTH)
+    type = models.CharField(max_length=20, choices=TYPES, default='text')
+
+    def __str__(self):
+        return self.text
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=TEXT_LENGTH)
+
+    def __str__(self):
+        return self.text
